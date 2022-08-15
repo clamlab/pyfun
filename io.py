e8c5f2fcf550d107d356ecdc8895f36528537531
edmundchong <edmund.w.chong@gmail.com>
2022-08-15 15:21:42 +0100

    io

---
+++ io.py
@@ -0,0 +1,22 @@
+import tkinter, tkinter.filedialog as tkfiledialog
+import numpy as np
+from glob import glob
+from ast import literal_eval
+
+
+def get_file(initial_dir=None, title=''):
+    root = tkinter.Tk()
+    root.after(100, root.focus_force)
+    root.after(200, root.withdraw)
+    f_path = tkfiledialog.askopenfilename(parent=root, initialdir=initial_dir, title=title)
+
+    return f_path
+
+
+def get_folder(initial_dir=None, title=''):
+    root = tkinter.Tk()
+    root.after(100, root.focus_force)
+    root.after(200, root.withdraw)
+    fd_path = tkfiledialog.askdirectory(parent=root, initialdir=initial_dir, title=title)
+
+    return fd_path
